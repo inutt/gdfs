f95027143b4d8eb4f7fbc5c8f01858ae48426325
inutt <richard.ankh@gmail.com>
2013-10-02 22:09:15 +0100

    Improved API class
      - Separated query parameters from the URL
      - Added debug parameter to see which URLs are being requested

--- lib/Google/API.pm
+++ lib/Google/API.pm
@@ -6,7 +6,9 @@ use common::sense;
 use LWP::UserAgent;
 use HTTP::Request;
 use HTTP::Headers;
+use URI::Escape;
 use JSON;
+use Carp;
 use Google::Auth;
 
 our $VERSION = "0.1";
@@ -19,22 +21,37 @@ sub new
 		auth => Google::Auth->new(%options),
 		api_base_url => 'https://www.googleapis.com/',
 		lwp => LWP::UserAgent->new(),
+		debug => $options{'debug'},
 	};
 
 	bless $this, $class;
 	return $this;
 };
 
+sub debug:lvalue { my $this = shift; return $this->{'debug'}; };
+
 sub request
 {
 	my $this = shift;
-	my $url = shift;
-	return undef if !$url;
+	my $url = shift || return undef;
+	my %params = @_;
 
 	# Ensure we have a valid access token, one way or another.
 	$this->{'auth'}->auth();
 
+	# Assemble the query parameters (if supplied)
+	$params{'prettyPrint'} = 'false'; # Disable pretty printing to reduce response size
+	my @params;
+	foreach my $key (keys %params)
+	{
+		push @params, $key."=".uri_escape($params{$key});
+	};
+	$url .= "?" . join('&',@params) if @params;
+
+	print STDERR "Requesting ".$this->{'api_base_url'}.$url."\n" if $this->debug;
+
 	# Make the request
+	# TODO: Check the LWP object accepts gzip compression
 	my $request = HTTP::Request->new(
 		GET => $this->{'api_base_url'}.$url,
 		HTTP::Headers->new(Authorization => $this->{'auth'}->token_type.' '.$this->{'auth'}->access_token)
@@ -48,8 +65,7 @@ sub request
 	}
 	else
 	{
-		print Dumper $response;
-		die "API request failed: ".$response->message;
+		croak "API request failed: ".decode_json($response->decoded_content)->{'error'}->{'message'};
 	};
 };
 
